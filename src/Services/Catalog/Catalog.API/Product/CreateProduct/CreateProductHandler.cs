7b40e7e53f94b1bc5021e556f2bf2a425c05fa8b
Anthony McEvoy <asmtony@hotmail.co.uk>
2026-02-07 21:36:49 +0000

    Setup CatalogAPI

---
+++ src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,38 @@
+﻿namespace Catalog.API.Product.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Categories,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+
+
+public record CreateProductResult(Guid Id);
+
+// Use martin for the database, but you can use any ORM or database access method you prefer
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // create product entity from command object
+        // Save to database,
+        // Return CreateProductResult
+        var product = new Models.Product
+        {
+            Name = command.Name,
+            Categories = command.Categories,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+
+        // Save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+        // return result
+        //return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
+    }
+}
